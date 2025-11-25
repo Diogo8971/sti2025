f08ffc820bf44ba7e4d0d5a7bc0d432c33c9ddd2
Diogo Martins <a89836@ualg.pt>
2025-11-25 15:22:00 +0000

    Deploy website - based on f3e76b7cc92d7b1b2c6749f47fca71a8c7e76681

---
+++ assets/js/acecf23e.0e45652b.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdokku=globalThis.webpackChunkdokku||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/sti2025/blog","blogTitle":"Blog","authorsListPath":"/sti2025/blog/authors"}')}}]);
---
+++ assets/js/b30ac7aa.456e1020.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdokku=globalThis.webpackChunkdokku||[]).push([[4131],{7829:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/sti2025/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
