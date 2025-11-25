682eb387a6600daf39d1e6dd0ac71d952d0eb04f
Diogo Martins <a89836@ualg.pt>
2025-11-25 15:54:05 +0000

    Alterei a tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'dokku',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Site de exemplo',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
